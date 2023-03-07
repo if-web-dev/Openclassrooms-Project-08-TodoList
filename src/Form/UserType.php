67f0531695f08b77fc3b45b88657f425d48edbda
ishake fouhal <i.fouhal@hotmail.com>
2023-03-07 22:42:46 +0100

    close #7, Choose a role for a user

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
@@ -22,6 +24,25 @@ class UserType extends AbstractType
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
-            ->add('email', EmailType::class, ['label' => 'Adresse email']);
+            ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => "ROLE_USER",
+                    'Administrateur' => "ROLE_ADMIN",
+                ],
+            ]);
+
+        $builder
+            ->get('roles')
+            ->addModelTransformer(
+                new CallbackTransformer(
+                    function ($rolesAsArray) {
+                        return count($rolesAsArray) ? $rolesAsArray[0] : null;
+                    },
+                    function ($rolesAsString) {
+                        return [$rolesAsString];
+                    }
+                )
+            );
     }
 }
