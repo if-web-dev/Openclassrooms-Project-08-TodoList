a470505f7b74ac0f352802d9acdb6b4861061b70
ishake fouhal <i.fouhal@hotmail.com>
2023-03-18 19:41:01 +0100

    feat: voter update

--- src/Security/Voter/TaskVoter.php
+++ src/Security/Voter/TaskVoter.php
@@ -8,13 +8,13 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class TaskVoter extends Voter
 {
-    public const DELETE = 'TASK_DELETE';
+    public const MODIFY = 'TASK_MODIFY';
 
     protected function supports(string $attribute, mixed $task): bool
     {
         // replace with your own logic
         // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::DELETE])
+        return in_array($attribute, [self::MODIFY])
             && $task instanceof \App\Entity\Task;
     }
 
@@ -25,11 +25,11 @@ class TaskVoter extends Voter
         if (!$user instanceof UserInterface) {
             return false;
         }
-        // Tasks attached to the “anonymous” user can only be deleted by users with the administrator role (ROLE_ADMIN).
+        // Tasks attached to the “anonymous” user can only be modified by users with the administrator role (ROLE_ADMIN).
         if ($user->getRoles()[0] === "ROLE_ADMIN"){
             return true;
         }
-        // Tasks can only be deleted by users who created the task in question.
+        // Tasks can only be modified by users who created the task in question.
         if ($user === $task->getUser()){
             return true;
         }
